827d022079e6457b805b741205fc7ac5bff337ca
Nkamenyin <valentineeyet01@gmail.com>
2025-08-30 14:43:06 +0100

    updated again

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,6 +1,7 @@
 -- Query to retrieve all bookings with user, property, and payment details
 
-SELECT 
+EXPLAIN
+SELECT
     b.booking_id,
     b.booking_date,
     b.check_in_date,
@@ -20,5 +21,7 @@ SELECT
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
-
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND p.city = 'New York'
+ORDER BY b.booking_date DESC;
